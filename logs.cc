d881f710261ac0755f5d959710a53927cd33aa9f
Ilya Khramtsov <i.khramtsov@i-dgtl.ru>
2020-03-03 16:16:03 +0300

    added date time and sleep 1s

--- logs.cc
+++ logs.cc
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <time.h>
+#include <unistd.h>
 using namespace std;
 
 int main () {
     while (true){
-        printf ("sdfsdf\n");
-        return 0;
+        time_t now_time = time(NULL); 
+        printf ("%s: This is a test journal.\n", ctime(&now_time));
+        sleep (1);
     }
 }
